ab4953b9148a8cce397910d6eb164dc1e2c90ea8
Pooja Jadav <contact2pooja36@gmail.com>
2024-07-23 11:40:16 +0530

    User seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'first@example.com',
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'second@example.com',
         ]);
     }
 }
